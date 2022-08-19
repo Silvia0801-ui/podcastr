d4d9cbd0809b886af7043c0d9fe5a327c20cbeb9
silvia <silviadeazevedoalvesgomes@gmail.com>
2022-08-19 11:12:24 -0300

    podcastr

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import styles from './styles.module.scss';
 
 export function Header () {
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR,
+  });
+
     return (
         <header className={styles.headerContainer}>
           <img src="/logo.svg" alt="Podcastr"/>
-          <p>O melhor para você ouvir, sempre</p>
+          
+          <p>O melhor para você ouvir, sempre.</p>
     
-          <span>kskskskksksksksks</span>
+          <span>{currentDate}</span>
         </header>
       )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
-import { Header } from '../components/Header'
 import '../styles/global.scss'
+
+import { Header } from '../components/Header'
+import { Player } from '../components/Player';
+
+import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
   return(
-  <div>
-    <Component {...pageProps} />
-    {/* <Header /> */}
-  </div>
+   <div className={styles.wrapper}>
+    <main>
+    <Header/>
+     <Component {...pageProps} />
+    </main>
+    <Player/>
+   </div>
   ) 
 }
 
